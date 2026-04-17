083be725f7506334b048fa8fd6423fd596527b04
chanakya <you@example.com>
2026-04-17 12:00:09 +0530

    shell-roboshop-common-funtions

---
+++ redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./common.sh
+app_name="redis"
+
+
+check_root
+
+
+
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "Installing Redis"
+
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOGS_FILE
+VALIDATE $? "Starting Redis"
+
+print_total_time
